1bd649fcef7b6c74516c1ad29c133fa8ec100c8b
Chinmaya-V <chinmayav04@gmail.com>
2023-12-30 14:14:55 +0530

    Update categories.constants.js

--- src/utils/constants/categories.constants.js
+++ src/utils/constants/categories.constants.js
@@ -11,8 +11,9 @@ export const CATEGORIES = [
   },
   {
     id: 3,
-    title: 'Trousers',
-    imageURL: 'https://images.pexels.com/photos/19614011/pexels-photo-19614011/free-photo-of-trousers.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'Shoes',
+    imageURL:
+      'https://images.pexels.com/photos/19679401/pexels-photo-19679401/free-photo-of-shoe-collections.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
   },
   {
     id: 4,
@@ -23,10 +24,5 @@ export const CATEGORIES = [
     id: 5,
     title: 'Women',
     imageURL: 'https://images.pexels.com/photos/19613667/pexels-photo-19613667/free-photo-of-women-s-t-shirt.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  },
-  {
-    id: 6,
-    title: 'Kids',
-    imageURL: 'https://images.pexels.com/photos/19613695/pexels-photo-19613695/free-photo-of-kids-wear.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
   },
 ]
