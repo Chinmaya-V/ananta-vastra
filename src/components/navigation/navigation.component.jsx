777f33cd9a6d7784644721c19bdac59ccfd88d6c
Chinmaya-V <chinmayav04@gmail.com>
2024-01-01 23:27:19 +0530

    Update Navigation Component

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Outlet, NavLink } from 'react-router-dom'
 import './navigation.styles.scss'
 import Logo from '../logo/logo.component'
 
 const NavigationBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
-
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768)
   const NAVIGATION_LINKS = [
     { path: '/shop', label: 'Shop' },
     { path: '/contact', label: 'Contact' },
@@ -21,24 +21,33 @@ const NavigationBar = () => {
       ))}
     </>
   )
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768)
+    setIsMenuOpen(false)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
-    <React.Fragment>
+    <>
       <div className="navigation-bar">
         <NavLink to={'/'}>
           <Logo />
         </NavLink>
-        {!isMenuOpen ? (
+        {!isMobile && (
           <div className="nav-links-container">
             <NavigationLinks />
           </div>
-        ) : (
-          <>
-            {isMenuOpen && (
-              <div className="mobile-menu">
-                <NavigationLinks />
-              </div>
-            )}
-          </>
+        )}
+        {isMobile && isMenuOpen && (
+          <div className="mobile-menu">
+            <NavigationLinks />
+          </div>
         )}
         <div className={`mobile-menu-icon ${isMenuOpen ? 'open' : ''}`} onClick={() => setIsMenuOpen((prev) => !prev)}>
           <div className="bar1"></div>
@@ -46,16 +55,16 @@ const NavigationBar = () => {
           <div className="bar3"></div>
         </div>
       </div>
-    </React.Fragment>
+    </>
   )
 }
 
 const Navigation = () => {
   return (
-    <React.Fragment>
+    <>
       <NavigationBar />
       <Outlet />
-    </React.Fragment>
+    </>
   )
 }
 
